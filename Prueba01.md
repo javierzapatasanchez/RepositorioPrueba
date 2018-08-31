bc58083fb7052436ff931c55d156a627443c1944
Javier Zapata Sanchez <javierzapatasanchez@gmail.com>
2018-08-31 14:46:43 -0500

    Primer cambio
    1. Esta es una descripción de prueba de el primer cambio

---
+++ Prueba01.md
@@ -0,0 +1,2 @@
+Prueba
+le estoy agregando algo mas y esto es algo mas
---
+++ dom/codigo.js
@@ -0,0 +1,5 @@
+var ruta=window.location;
+var prueba_navigator=window;
+console.log(ruta);
+document.write("Estás en la siguiente ruta:" + ruta);
+alert("si funciona");
